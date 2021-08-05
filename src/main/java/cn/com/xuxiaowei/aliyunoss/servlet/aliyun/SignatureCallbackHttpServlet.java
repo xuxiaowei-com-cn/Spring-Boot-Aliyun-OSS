531e1da3dfb79e91636dc9ac031de11f6da20219
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-08-06 07:53:03 +0800

    回调参数

--- src/main/java/cn/com/xuxiaowei/aliyunoss/servlet/aliyun/SignatureCallbackHttpServlet.java
+++ src/main/java/cn/com/xuxiaowei/aliyunoss/servlet/aliyun/SignatureCallbackHttpServlet.java
@@ -5,6 +5,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 阿里云回调服务器
@@ -18,6 +21,12 @@ public class SignatureCallbackHttpServlet extends HttpServlet {
 
         System.err.println("回调服务器GET");
 
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        Set<Map.Entry<String, String[]>> entries = parameterMap.entrySet();
+        for (Map.Entry<String, String[]> entrie : entries) {
+            System.out.println(entrie.getKey() + "\t" + Arrays.toString(entrie.getValue()));
+        }
+
         resp.getWriter().println("{\"Status\":\"OK\",\"method\":\"GET\"}");
         resp.flushBuffer();
 
@@ -27,6 +36,12 @@ public class SignatureCallbackHttpServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         System.err.println("回调服务器POST");
+
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        Set<Map.Entry<String, String[]>> entries = parameterMap.entrySet();
+        for (Map.Entry<String, String[]> entrie : entries) {
+            System.out.println(entrie.getKey() + "\t" + Arrays.toString(entrie.getValue()));
+        }
 
         resp.getWriter().println("{\"Status\":\"OK\",\"method\":\"POST\"}");
         resp.flushBuffer();
